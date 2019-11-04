f3cca54a9f725f1de5de21afcde29785cbbfa0aa
arc0801 <cjdfusdnjfgk@gmail.com>
2019-11-04 12:50:45 +0900

    11/4 am

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -8,6 +8,10 @@
 </head>
 <body>
 	<h1>Index Page</h1>
-	<a href="./notice/noticeList.do">Go Notice</a> <!-- 확장자 아무 의미 없댜 -->
+	<a href="./notice/noticeList.notice">Go Notice</a> <!-- 확장자 아무 의미 없댜 -->
+	<!-- <a href="./WEB-INF/test.jsp">Go test</a> 직접 주소창에 쳐서 들어가지는 못한다. 서버 내 즉, backend단에서만 접근가능 -->
+	<a href="./notice/noticeSelect.notice?check=1">Go Notice Select</a>
+	<a href="./member/memberList.do">Go Member</a>
+	<!-- Member Controller -->
 </body>
 </html>
--- src/com/arc/controller/NoticeController.java
+++ src/com/arc/controller/NoticeController.java
@@ -1,6 +1,9 @@
 package com.arc.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -28,6 +31,68 @@ public class NoticeController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		System.out.println("Notice Controller");
+		
+		String url = request.getServletPath();
+		
+		//1. if(url.equals("/notice/noticeList.notice")) {};
+		
+		
+		//System.out.println(url);
+		
+		//String[] a = url.split("/");
+		//for(int i=0;i<a.length;i++) {
+		//	System.out.println(a[i]);
+		//}
+		
+		//url = a[2].substring(0, a[2].lastIndexOf("."));
+		
+		url = url.substring(url.lastIndexOf("/")+1, url.lastIndexOf("."));
+		System.out.println(url);
+		
+		String uri = request.getRequestURI();
+		StringBuffer rurl = request.getRequestURL();
+		
+		System.out.println(request.getServletPath()); //차이점!!!!!!!!!!
+		System.out.println(uri);
+		System.out.println(rurl);
+		
+		//2.
+		String path = "";
+		if(url.equals("noticeList")) {
+			path = "./noticeList.jsp";
+			request.setAttribute("board", "noticeList");
+		}else if(url.equals("noticeSelect")){
+			String check = request.getParameter("check");
+			//check 1이라면 select
+			//아니라면 list
+			if(check.equals("1")) {
+				path = "./noticeSelect.jsp";
+				request.setAttribute("select", "noticeSelect");
+			}else {
+				response.sendRedirect("./noticeList.notice");
+			}
+			
+		}
+		
+		RequestDispatcher view = request.getRequestDispatcher(path);
+		view.forward(request, response);
+		/*
+		 * String command = request.getParameter("command");
+		 * 
+		 * String path = "";
+		 * 
+		 * if(command.equals("list")) { path = "./noticeList.jsp";
+		 * request.setAttribute("board", "notice"); }else { path = "./noticeSelect.jsp";
+		 * }
+		 * 
+		 * 
+		 * //PrintWriter out = response.getWriter(); out.print("<html>");
+		 * 
+		 * RequestDispatcher view = request.getRequestDispatcher(path);
+		 * view.forward(request, response);
+		 * 
+		 * //response.sendRedirect("./noticeList.jsp");
+		 */
 	}
 
 	/**
